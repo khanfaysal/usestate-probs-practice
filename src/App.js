bece2982f91be8f1bb5ae9e4baf3f3ac42b27716
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-26 17:16:22 +0600

    state and probs practice in react fundamental

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <TicketCounter></TicketCounter>
+        
       </header>
     </div>
   );
 }
+const TicketCounter = () =>{
+  const [count , setCount] = useState(0);
+  const buttonHandler = () => setCount(count+1);
+  return (
+    <div>
+      <button onClick = {buttonHandler}>Add Ticket</button>
+      <h3>Show Ticket Number: {count} </h3>
+      <TicketDisplay ticket = {count}></TicketDisplay>
+    </div>
+  )
+}
+const TicketDisplay = (props) =>{
+  return (
+    <div>
+      <h4>Ticket Display Number: {props.ticket} </h4>
+    </div>
+  )
+}
+
+
+
 
 export default App;
